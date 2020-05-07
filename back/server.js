7aa696b13bea0646fdb742f1a5b016a4f7268b07
jlguenego@gmail.com <jlguenego@gmail.com>
2020-05-07 11:35:30 +0200

    serveur statique

--- back/server.js
+++ back/server.js
@@ -1,13 +1,17 @@
 const express = require("express");
+const serveIndex = require("serve-index");
 
 const app = express();
 
-app.get("/toto", (req, res) => {
-  res.send("Hello JL!");
+app.use((req, res, next) => {
+  console.log("on a recu un appel");
+  next();
 });
+
+app.use(express.static('public'));
+app.use(serveIndex('public'));
+
 
 app.listen(3000, () => {
   console.log("server started with success");
 });
-
-console.log("coucou");
