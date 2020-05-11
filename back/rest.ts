d38283287b89456541c18d4fbc853a04e250e98e
Jean-Louis GUENEGO <jlguenego@gmail.com>
2020-05-11 10:24:58 +0200

    crud ok

--- back/rest.ts
+++ back/rest.ts
@@ -1,23 +1,23 @@
-import express from 'express';
+import express from "express";
 
 const app = express.Router();
 
 app.use(express.json());
 
 const articles = [
-  { id: 0, name: 'paire de chaussette', prix: '32' },
-  { id: 1, name: 'paire de chaussure', prix: '310' },
-  { id: 2, name: 'masque', prix: '10' },
-  { id: 3, name: 'slip', prix: '5' },
+  { id: 0, name: "paire de chaussette", prix: "32" },
+  { id: 1, name: "paire de chaussure", prix: "310" },
+  { id: 2, name: "masque", prix: "10" },
+  { id: 3, name: "slip", prix: "5" },
 ];
 
 const obj = { id: 4 };
 
-app.get('/articles', (req, res) => {
+app.get("/articles", (req, res) => {
   res.json(articles);
 });
 
-app.get('/articles/:myNiceId', (req, res) => {
+app.get("/articles/:myNiceId", (req, res) => {
   const i = req.params.myNiceId;
   if (articles[i] === undefined) {
     res.status(404).end();
@@ -26,7 +26,7 @@ app.get('/articles/:myNiceId', (req, res) => {
   res.json(articles[i]);
 });
 
-app.post('/articles', (req, res) => {
+app.post("/articles", (req, res) => {
   const article = req.body;
   article.id = obj.id;
   obj.id++;
@@ -34,8 +34,48 @@ app.post('/articles', (req, res) => {
   res.status(201).json(article);
 });
 
-app.delete('/articles', (req, res) => {
+app.delete("/articles", (req, res) => {
   articles.length = 0;
+  res.status(204).end();
+});
+
+app.delete("/articles/:id", (req, res) => {
+  const id = +req.params.id;
+  const index = articles.findIndex((a) => a.id === id);
+  if (index === -1) {
+    return res.status(404).end();
+  }
+  articles.splice(index, 1);
+  res.status(204).end();
+});
+
+app.put("/articles/:id", (req, res) => {
+  // /TODO: if (!check(req.body) ) {
+  //   return res.status(400).end();
+  // }
+  const id = +req.params.id;
+  const index = articles.findIndex((a) => a.id === id);
+  if (index === -1) {
+    return res.status(404).end();
+  }
+  const article = { id, ...req.body };
+  articles.splice(index, 1, article);
+  res.status(204).end();
+});
+
+app.patch("/articles/:id", (req, res) => {
+  // /TODO: if (!check(req.body) ) {
+  //   return res.status(400).end();
+  // }
+  const id = +req.params.id;
+  const article = articles.find((a) => a.id === id);
+  if (!article) {
+    return res.status(404).end();
+  }
+  Object.keys(req.body).forEach((prop) => {
+    article[prop] = req.body[prop];
+  });
+  
   res.status(204).end();
 });
 
