3bffee82bd3d65f6a49c099c2c9c99e765304f1a
Jean-Louis GUENEGO <jlguenego@gmail.com>
2020-05-11 09:35:52 +0200

    add delete

--- back/rest.ts
+++ back/rest.ts
@@ -1,23 +1,42 @@
-import express from "express";
+import express from 'express';
 
 const app = express.Router();
 
 app.use(express.json());
 
 const articles = [
-  { name: "paire de chaussette", prix: "32" },
-  { name: "paire de chaussure", prix: "310" },
-  { name: "masque", prix: "10" },
-  { name: "slip", prix: "5" },
+  { id: 0, name: 'paire de chaussette', prix: '32' },
+  { id: 1, name: 'paire de chaussure', prix: '310' },
+  { id: 2, name: 'masque', prix: '10' },
+  { id: 3, name: 'slip', prix: '5' },
 ];
 
-app.get("/articles", (req, res) => {
+const obj = { id: 4 };
+
+app.get('/articles', (req, res) => {
   res.json(articles);
 });
 
-app.get("/articles/:myNiceId", (req, res) => {
+app.get('/articles/:myNiceId', (req, res) => {
   const i = req.params.myNiceId;
+  if (articles[i] === undefined) {
+    res.status(404).end();
+    return;
+  }
   res.json(articles[i]);
+});
+
+app.post('/articles', (req, res) => {
+  const article = req.body;
+  article.id = obj.id;
+  obj.id++;
+  articles.push(article);
+  res.status(201).json(article);
+});
+
+app.delete('/articles', (req, res) => {
+  articles.length = 0;
+  res.status(204).end();
 });
 
 export const rest = app;
