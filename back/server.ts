2c60f7dae572493334f84083fb2460ff4caa3de8
Jean-Louis GUENEGO <jlguenego@gmail.com>
2020-05-11 12:04:36 +0200

    added mongo

--- back/server.ts
+++ back/server.ts
@@ -1,10 +1,15 @@
 import express from "express";
 import serveIndex from "serve-index";
 import { ws } from "./ws";
+import { MongoClient } from "mongodb";
+
+const url = "mongodb://localhost:27017";
+const dbName = "gestion-stock";
+const client = new MongoClient(url, { useUnifiedTopology: true });
 
 const app = express();
 
-app.use('/ws', ws);
+app.use("/ws", ws);
 
 app.use((req, res, next) => {
   console.log("on a recu un appel");
@@ -14,6 +19,17 @@ app.use((req, res, next) => {
 app.use(express.static("public"));
 app.use(serveIndex("public"));
 
-app.listen(3000, () => {
-  console.log("server started with success");
-});
+(async function () {
+  try {
+    await client.connect();
+    console.log("Connected successfully to the database server");
+
+    const db = client.db(dbName);
+
+    app.listen(3000, () => {
+      console.log("server started with success");
+    });
+  } catch (err) {
+    console.log("err: ", err);
+  }
+})();
