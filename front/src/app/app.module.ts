7289a58bb9aa154d9abeb9bc9185a600d8378d96
Jean-Louis GUENEGO <jlguenego@gmail.com>
2020-05-22 16:30:38 +0200

    added routing

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -4,10 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LayoutModule } from './layout/layout.module';
+import { HomeComponent } from './routes/home/home.component';
+import { LegalComponent } from './routes/legal/legal.component';
+import { ArticlesComponent } from './routes/articles/articles.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LegalComponent,
+    ArticlesComponent
   ],
   imports: [
     BrowserModule,
