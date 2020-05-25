d51d606ff06bf0ec3d1c7919f8a5ffcff421ae4e
Jean-Louis GUENEGO <jlguenego@gmail.com>
2020-05-25 16:56:06 +0200

    select ok

--- front/src/app/interfaces/article.ts
+++ front/src/app/interfaces/article.ts
@@ -1,4 +1,5 @@
 export interface Article {
+  _id?: string;
   name: string;
   price: number;
 }
--- front/src/app/routes/articles/articles.component.ts
+++ front/src/app/routes/articles/articles.component.ts
@@ -7,13 +7,29 @@ import { ArticleService } from 'src/app/services/article.service';
   styleUrls: ['./articles.component.scss'],
 })
 export class ArticlesComponent implements OnInit {
-
+  selectedList: string[] = [];
   constructor(public articleService: ArticleService) {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   select(event: MouseEvent) {
     console.log('select', event);
+    const row = (event.target as HTMLElement).classList.contains('row')
+      ? (event.target as HTMLElement)
+      : (event.target as HTMLElement).parentElement;
+    const cl = row.classList;
+    const id = row.getAttribute('id');
+    if (cl.contains('selected')) {
+      this.selectedList.splice(
+        this.selectedList.findIndex((item) => item === id),
+        1
+      );
+      cl.remove('selected');
+      console.log('this.selectedList: ', this.selectedList);
+      return;
+    }
+    this.selectedList.push(id);
+    cl.add('selected');
+    console.log('this.selectedList: ', this.selectedList);
   }
 }
