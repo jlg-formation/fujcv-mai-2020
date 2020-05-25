fb3999eed1c4464e7c3b0cb1a3fdcf618333423e
Jean-Louis GUENEGO <jlguenego@gmail.com>
2020-05-25 11:16:50 +0200

    link form

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -21,6 +23,7 @@ import { NewArticleComponent } from './routes/new-article/new-article.component'
   imports: [
     BrowserModule,
     HttpClientModule,
+    ReactiveFormsModule,
     AppRoutingModule,
     LayoutModule
   ],
--- front/src/app/routes/new-article/new-article.component.ts
+++ front/src/app/routes/new-article/new-article.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-new-article',
   templateUrl: './new-article.component.html',
-  styleUrls: ['./new-article.component.scss']
+  styleUrls: ['./new-article.component.scss'],
 })
 export class NewArticleComponent implements OnInit {
+  f = new FormGroup({
+    name: new FormControl('toto', [Validators.required]),
+    price: new FormControl(12.34, [Validators.required]),
+  });
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
